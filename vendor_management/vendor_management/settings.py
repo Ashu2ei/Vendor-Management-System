01d42df85f309e089244ba18d9c252b402b8da9d
ashutosh <ashutosh20.mishra@ril.com>
2024-01-25 17:31:29 +0530

    hello

--- vendor_management/vendor_management/settings.py
+++ vendor_management/vendor_management/settings.py
@@ -130,10 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #STATIC_URL = '/static/'
-import os
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# import os
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
