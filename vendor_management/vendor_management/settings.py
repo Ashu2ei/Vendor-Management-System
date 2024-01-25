8dc0673956096056026b927d997f1c91e97ce158
ashutosh <ashutosh20.mishra@ril.com>
2024-01-25 15:07:35 +0530

    database changes

--- vendor_management/vendor_management/settings.py
+++ vendor_management/vendor_management/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'vendor_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'backendcourse',
+        'USER': 'postgres',
+        'PASSWORD': 'ashu1995',
+        'HOST': 'backendcourse.cfqi64saifvr.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
